7763afd46d1c8b01953da6a34ceda33877d0d878
David Millman <dave@cs.unc.edu>
2015-10-11 17:36:13 -0400

    Add local and global searchs

--- zsh/zle.zsh
+++ zsh/zle.zsh
@@ -23,3 +23,35 @@ function _fzf-fo-widget() {
 zle -N _fzf-fo-widget
 bindkey '\eo' _fzf-fo-widget
 
+## History awesomeness
+# cursor and the ctrl+N and ctrl+P moves through local history
+# ctrl+cursro moves through global history
+# ctrl+R will still search over global history
+function _up-line-or-local-history() {
+    zle set-local-history 1
+    zle up-line-or-history
+    zle set-local-history 0
+}
+zle -N _up-line-or-local-history
+
+function _down-line-or-local-history() {
+    zle set-local-history 1
+    zle down-line-or-history
+    zle set-local-history 0
+}
+zle -N _down-line-or-local-history
+
+# map local up
+up_key=${terminfo[kcuu1]}
+bindkey "${up_key}" _up-line-or-local-history       # cursor up
+bindkey "^P" _up-line-or-local-history              # ctrl+p
+
+# map local down
+down_key=${terminfo[kcud1]}
+bindkey "${down_key}" _down-line-or-local-history   # cursor down
+bindkey "^N" _down-line-or-local-history            # ctrl+n
+
+# map global
+bindkey "^[[1;5A" up-line-or-history                # ctrl+Cursor up
+bindkey "^[[1;5B" down-line-or-history              # ctrl+Cursor down
+
