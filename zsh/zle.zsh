184ee853cda0f38d7033101b949a8be8866edffd
David Millman <dave@production.pro>
2015-10-11 12:50:24 -0400

    Add zle configuation
    This adds a widgeted that uses fzf make it easy to opening files in the
    editor or externally.  It is mapped to alt+o

---
+++ zsh/zle.zsh
@@ -0,0 +1,25 @@
+# Additional bindings
+#
+### FZF awesomeness
+# fe [FUZZY PATTERN] - Open the selected file with the default editor
+#   - Exit if there's no match (--exit-0)
+#   - CTRL-O to open with `open` command,
+#   - CTRL-E or Enter key to open with the $EDITOR
+fo() {
+  local out file key
+  out=$(fzf-tmux --query="$1" --exit-0 --expect=ctrl-o,ctrl-e)
+  key=$(head -1 <<< "$out")
+  file=$(head -2 <<< "$out" | tail -1)
+  if [ -n "$file" ]; then
+    [ "$key" = ctrl-o ] && open "$file" || ${EDITOR:-vim} "$file"
+  fi
+}
+
+function _fzf-fo-widget() {
+    zle kill-whole-line
+    BUFFER="fo"
+    zle accept-line
+}
+zle -N _fzf-fo-widget
+bindkey '\eo' _fzf-fo-widget
+
