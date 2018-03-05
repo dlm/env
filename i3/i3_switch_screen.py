1d639bd04f04b974d5820e2975cbe724a40fb989
David Millman <dave@cs.unc.edu>
2018-03-04 21:09:20 -0700

    Remove unused script

--- i3/i3_switch_screen.py
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env python
-
-import i3
-current = [ws for ws in i3.get_workspaces() if ws['focused']][0]
-other = [ws for ws in i3.get_workspaces() if ws['visible'] and not ws['focused']][0]
-i3.workspace(other['name'])
