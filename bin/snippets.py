4bc186f62f885ec1abb2bd1bff9f67512f6d6e16
David Millman <dave@cs.unc.edu>
2018-03-13 16:30:55 -0600

    Add snippets.py

---
+++ bin/snippets.py
@@ -0,0 +1,42 @@
+#!/usr/bin/env python
+from __future__ import print_function
+
+import sys
+import subprocess
+import datetime
+
+snippets = {
+    "office-hours": (
+        "Mon 17:00-18:00\n"
+        "Thurs 12:00-13:00"
+    ),
+    "date (YYYY-mm-dd)": (
+        datetime.datetime.now().strftime("%Y-%m-%d")
+    ),
+    "date (MMMM dd, YYYY)": (
+        datetime.datetime.now().strftime("%B %e, %Y")
+    ),
+    "discussion-board-code-help": (
+        "I am happy to help.  You are welcome to come by office hours any time "
+        "or post on the discussion board.  (I prefer the board over direct "
+        "email because it may help a few others out that are having the same "
+        "problem).  If you go the route of posting, please add a few "
+        "additional pieces of information such as the error message that you "
+        "are getting, where you are getting the error, your OS, your "
+        "development environment and, if you can find it, the graphics driver "
+        "and graphics card information."
+    ),
+}
+
+
+def main():
+    if len(sys.argv) >= 2:
+        value = snippets.get(sys.argv[1], "no entry")
+        print(value)
+    else:
+        for key in snippets.keys():
+            print(key)
+
+
+if __name__ == '__main__':
+    main()
