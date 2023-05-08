109fa94a020fb9310fec204b9125f1262d369fa1
David Millman <dave@cs.unc.edu>
2023-05-07 20:25:06 -0600

    Add awesome copy shortcut

--- nvim/lua/dlm/remap.lua
+++ nvim/lua/dlm/remap.lua
@@ -9,6 +9,10 @@ vim.keymap.set("n", "<C-k>", "<C-W>k")
 vim.keymap.set("n", "<C-h>", "<C-W>h")
 vim.keymap.set("n", "<C-l>", "<C-W>l")
 
+-- next greatest remap ever : asbjornHaland
+vim.keymap.set({"n", "v"}, "<leader>y", [["+y]])
+vim.keymap.set("n", "<leader>Y", [["+Y]])
+
 -- clean up highlight after searching
 vim.keymap.set("n", "<cr>", ":nohlsearch<cr>")
 
