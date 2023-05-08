24ada3d68c43cff300c49bc386d69d86e83ca6d8
David Millman <dave@cs.unc.edu>
2023-05-07 20:26:58 -0600

    Set richgo as test runner

--- nvim/after/plugin/vim-test.lua
+++ nvim/after/plugin/vim-test.lua
@@ -2,4 +2,5 @@ vim.g['test#strategy'] = "basic"
 vim.keymap.set("n", "<leader>tt", ":TestNearest<CR>", { silent = true })
 vim.keymap.set("n", "<leader>tf", ":TestFile<CR>", { silent = true })
 
-
+vim.g['test#go#runner'] = 'richgo'
+vim.g['test#go#richgo#options'] = ' '
