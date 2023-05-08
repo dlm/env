4e1f9388d9148cbdd9648f869e6a1f3b1e9ac0e3
David Millman <dave@cs.unc.edu>
2023-05-07 20:24:41 -0600

    Add some snips

--- nvim/after/plugin/lsp.lua
+++ nvim/after/plugin/lsp.lua
@@ -12,11 +12,15 @@ lsp.ensure_installed({
 
 local cmp = require('cmp')
 local cmp_select = {behavior = cmp.SelectBehavior.Select}
+local cmp_action = require('lsp-zero').cmp_action()
 local cmp_mappings = lsp.defaults.cmp_mappings({
 	['<C-p>'] = cmp.mapping.select_prev_item(cmp_select),
 	['<C-n>'] = cmp.mapping.select_next_item(cmp_select),
 	['<C-y>'] = cmp.mapping.confirm({ select = true }),
 	['<C-Space>'] = cmp.mapping.complete(),
+    ['<C-f>'] = cmp_action.luasnip_jump_forward(),
+    ['<C-b>'] = cmp_action.luasnip_jump_backward(),
+
 })
 
 lsp.setup_nvim_cmp({
---
+++ nvim/after/plugin/luasnip.lua
@@ -0,0 +1,51 @@
+local ls = require("luasnip")
+
+local s = ls.snippet
+local t = ls.text_node
+local i = ls.insert_node
+-- local f = ls.function_node
+-- local fmt = require("luasnip.extras.fmt").fmt
+local rep = require("luasnip.extras").rep
+local ps = ls.parser.parse_snippet
+
+
+-- args is a table, where 1 is the text in Placeholder 1, 2 the text in
+-- placeholder 2,...
+local function copy(args)
+	return args[1]
+end
+
+ls.add_snippets("all", {
+	ps({
+        trig = "trun",
+        namr = "T.Run",
+        dscr = "create a t.Run for go"
+    }, [[
+		t.Run("${1}", func(t *testing.T) {
+			${0}
+		})
+	]]),
+
+	ps({
+        trig = "truntodo",
+        namr = "T.Run TODO",
+        dscr = "create a t.Run with a t.FailNow("
+    }, [[
+		t.Run(${0}, func(t *testing.T) {
+		    assert.FailNow(t, "todo")
+		})
+	]]),
+
+    s({
+        trig = "bky-jira-link",
+        namr = "Blocky Jira Link",
+        dscr = "create a link to a blocky jira ticket the ticket number"
+    }, {
+        t("[bky-"),
+        i(1, "ticket number"),
+        t("](https://blocky.atlassian.net/browse/BKY-"),
+        rep(1),
+        t(")"),
+    }),
+})
+
