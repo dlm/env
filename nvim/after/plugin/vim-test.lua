afae9122a11d510d58fe31e0869676224fbd47e9
David Millman <dave@cs.unc.edu>
2024-01-11 08:37:43 -0700

    Use verbose tests in vim test

--- nvim/after/plugin/vim-test.lua
+++ nvim/after/plugin/vim-test.lua
@@ -3,4 +3,4 @@ vim.keymap.set("n", "<leader>tt", ":TestNearest<CR>", { silent = true })
 vim.keymap.set("n", "<leader>tf", ":TestFile<CR>", { silent = true })
 
 vim.g['test#go#runner'] = 'richgo'
-vim.g['test#go#richgo#options'] = ' '
+vim.g['test#go#richgo#options'] = '-v'
