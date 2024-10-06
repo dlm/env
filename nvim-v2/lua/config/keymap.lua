ec676c6ed51087eda487b96e330c7d5dfc75c2b3
David Millman <dave@cs.unc.edu>
2024-10-06 17:58:20 -0600

    Move over keymaps

--- nvim-v2/lua/config/keymap.lua
+++ nvim-v2/lua/config/keymap.lua
@@ -6,6 +6,12 @@ vim.keymap.set("n", "<C-k>", "<C-W>k")
 vim.keymap.set("n", "<C-h>", "<C-W>h")
 vim.keymap.set("n", "<C-l>", "<C-W>l")
 
+-- clear the search highlighting
+vim.keymap.set("n", "??", ":nohlsearch<cr>")
+
 -- next greatest remap ever : asbjornHaland
 vim.keymap.set({ "n", "v" }, "<leader>y", [["+y]])
 vim.keymap.set("n", "<leader>Y", [["+Y]])
+
+-- "special projects"
+vim.keymap.set("n", "<leader>pe", ":e! ~/.config/nvim-v2/.<cr>")
