abd19987a59779736cc0c9d24d3195d71ce01537
David Millman <dave@cs.unc.edu>
2024-11-17 09:56:43 -0700

    Update telescope for better mgmt

--- nvim-v2/lua/plugins/telescope.lua
+++ nvim-v2/lua/plugins/telescope.lua
@@ -1,18 +1,37 @@
 return {
-    'nvim-telescope/telescope.nvim',
+	"nvim-telescope/telescope.nvim",
 
-    tag = '0.1.8',
+	tag = "0.1.8",
 
-    depends = { 'nvim-lua/plenary.nvim' },
+	depends = { "nvim-lua/plenary.nvim" },
 
-    config = function()
-        require('telescope').setup { }
+	config = function()
+		local builtin = require("telescope.builtin")
+		local actions = require("telescope.actions")
 
-        local builtin = require('telescope.builtin')
-        vim.keymap.set('n', '<C-p>', builtin.git_files, {})
-        vim.keymap.set('n', '<leader>ff', builtin.find_files, {})
-        vim.keymap.set('n', '<leader>fg', builtin.live_grep, {})
-        vim.keymap.set('n', '<leader>fb', builtin.buffers, {})
-        vim.keymap.set('n', '<leader>fh', builtin.help_tags, {})
-    end
+		require("telescope").setup({
+			pickers = {
+				buffers = {
+					show_all_buffers = true,
+					sort_lastused = true,
+					theme = "dropdown",
+					previewer = false,
+					mappings = {
+						i = {
+							["<c-d>"] = actions.delete_buffer,
+						},
+						n = {
+							["dd"] = actions.delete_buffer,
+						},
+					},
+				},
+			},
+		})
+
+		vim.keymap.set("n", "<C-p>", builtin.git_files, {})
+		vim.keymap.set("n", "<leader>ff", builtin.find_files, {})
+		vim.keymap.set("n", "<leader>fg", builtin.live_grep, {})
+		vim.keymap.set("n", "<leader>fb", builtin.buffers, {})
+		vim.keymap.set("n", "<leader>fh", builtin.help_tags, {})
+	end,
 }
