1bb8caf88624eb13439f4794fc9e1025013d27ae
David Millman <dave@cs.unc.edu>
2024-11-29 12:37:09 -0700

    Set the shift width for nix

--- nvim/lua/config/options.lua
+++ nvim/lua/config/options.lua
@@ -7,6 +7,10 @@ vim.opt.tabstop = 4
 vim.opt.softtabstop = 4
 vim.opt.shiftwidth = 4
 vim.opt.expandtab = true
+vim.api.nvim_create_autocmd("FileType", {
+	pattern = "nix",
+	command = "setlocal shiftwidth=2 tabstop=2",
+})
 
 vim.opt.wrap = false
 
